4bda153dc6f2eb984405f40a48093adf61648195
GregTheLime <grisha.uryash@gmail.com>
2024-09-25 01:02:05 +0300

    Initial commit!

---
+++ src/main/java/net/gregthelime/eternalalchemy/EternalModClient.java
@@ -0,0 +1,10 @@
+package net.gregthelime.eternalalchemy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EternalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
